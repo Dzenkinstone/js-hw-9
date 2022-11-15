1d501af5ba731f77c0e33464829e5115f8385b30
Dzenkinstone <kozlovdanil6@gmail.com>
2022-11-15 11:55:55 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,12 +2,6 @@ import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
 
-const refs = {
-  delay: form.elements.delay.value,
-  step: form.elements.step.value,
-  amount: form.elements.amount.value,
-};
-
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
@@ -23,12 +17,12 @@ function createPromise(position, delay) {
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  refs.delay = e.currentTarget.delay.value;
-  refs.step = e.currentTarget.step.value;
-  refs.amount = e.currentTarget.amount.value;
+  let delay = Number(e.currentTarget.elements.delay.value);
+  let step = Number(e.currentTarget.elements.step.value);
+  let amount = Number(e.currentTarget.elements.amount.value);
 
-  for (let i = 1; i <= refs.amount; i += 1) {
-    createPromise(i, refs.delay)
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(
           `✅ Fulfilled promise ${position} in ${delay}ms`
@@ -39,5 +33,6 @@ form.addEventListener('submit', e => {
           `✅ Fulfilled promise ${position} in ${delay}ms`
         );
       });
+    delay += step;
   }
 });
